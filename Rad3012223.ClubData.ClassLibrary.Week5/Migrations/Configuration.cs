703e5d60d40a30146eda61fd8e36f14633fdd568
Martin-Melody <78037342+Martin-Melody@users.noreply.github.com>
2022-10-26 09:30:03 +0100

    Week 5 Database initialised.

---
+++ Rad3012223.ClubData.ClassLibrary.Week5/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace Rad3012223.ClubData.ClassLibrary.Week5.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Tracker.WebAPIClient;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Rad3012223.ClubData.ClassLibrary.Week5.Models.Week5ClubContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Rad3012223.ClubData.ClassLibrary.Week5.Models.Week5ClubContext context)
+        {
+            ActivityAPIClient.Track(StudentID: "S00211628", StudentName: "Martin Melody", activityName: "RAD301 Week5Lab 2223", Task: "Creating ClubDB Database");
+        }
+    }
+}
---
+++ Rad3012223.MVC.Week5/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace Rad3012223.MVC.Week5.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Tracker.WebAPIClient;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Rad3012223.MVC.Week5.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Rad3012223.MVC.Week5.Models.ApplicationDbContext context)
+        {
+            ActivityAPIClient.Track(StudentID: "S00211628", StudentName: "Martin Melody", activityName: "RAD301 Week5Lab 2223", Task: "Creating Application DB Database");
+        }
+    }
+}
