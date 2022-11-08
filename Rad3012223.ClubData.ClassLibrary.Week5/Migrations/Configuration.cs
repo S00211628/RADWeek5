fdceec0713e06e72ed9ecf9857515f57bc436f8b
Martin-Melody <78037342+Martin-Melody@users.noreply.github.com>
2022-11-08 15:39:22 +0000

    Week 5 Club Admin Controller Created

--- Rad3012223.ClubData.ClassLibrary.Week5/Migrations/Configuration.cs
+++ Rad3012223.ClubData.ClassLibrary.Week5/Migrations/Configuration.cs
@@ -54,48 +54,131 @@
             context.Student.AddOrUpdate(s => s.StudentID, students.ToArray());
             context.SaveChanges();
 
+            addClubs(context);
 
-            for (int i = 0; i < 10; i++)
-            {
 
-                if (i == 0)
-                {
-                    members.Add(
-                        new Member
-                        {
-                            MemberID = i,
-                            approved = true,
-                            StudentID = students[i].StudentID,
-                            AssociatedClub = 1,
-                        });
-                    i++;
-                }
-                    members.Add(
-                        new Member
-                        {
-                            MemberID = i,
-                            approved = false,
-                            StudentID = students[i].StudentID,
-                            AssociatedClub = 1,
-                        });
-            }
+            //for (int i = 0; i < 10; i++)
+            //{
+
+            //    if (i == 0)
+            //    {
+            //        members.Add(
+            //            new Member
+            //            {
+            //                MemberID = i,
+            //                approved = true,
+            //                StudentID = students[i].StudentID,
+            //                AssociatedClub = 1,
+            //            });
+            //        i++;
+            //    }
+            //        members.Add(
+            //            new Member
+            //            {
+            //                MemberID = i,
+            //                approved = false,
+            //                StudentID = students[i].StudentID,
+            //                AssociatedClub = 1,
+            //            });
+            //}
 
                 
             
-            context.Member.AddOrUpdate(m => m.MemberID, members.ToArray());
-            context.SaveChanges();
+            //context.Members.AddOrUpdate(m => m.MemberID, members.ToArray());
+            //context.SaveChanges();
 
-            context.Club.AddOrUpdate(c => c.ClubId, new Models.Club[]
-                {
-                    new Models.Club{ClubId = 1, ClubName = "Swimming Club", CreationDate = new DateTime(year:2019, month:12,day:9), adminID=93, clubMembers = members }
-                });
+            //context.Clubs.AddOrUpdate(c => c.ClubId, new Models.Club[]
+            //    {
+            //        new Models.Club{ClubId = 1, ClubName = "Swimming Club", CreationDate = new DateTime(year:2019, month:12,day:9), adminID=93, clubMembers = members }
+            //    });
 
-            context.SaveChanges();
+            //context.SaveChanges();
 
 
 
         }
 
+        private void addClubs(Week5ClubContext context)
+        {
+            List<Member> members = new List<Member>();
+            members = get_members(context);
+
+            //List<Member> club1 = new List<Member>();
+            //List<Member> club2 = new List<Member>();
+            //List<Member> club3 = new List<Member>();
+
+            //members.ForEach(rec =>
+            //{
+            //    if (rec.AssociatedClub == 1)
+            //    {
+            //        club1.Add(rec);
+            //    }
+            //    else if (rec.AssociatedClub == 2)
+            //    {
+            //        club2.Add(rec);
+            //    }
+            //    else if(rec.AssociatedClub == 3)
+            //    {
+            //        club3.Add(rec);
+            //    }
+            //});
+
+
+            Console.WriteLine("here");
+            for (int i = 0; i < members.Count; i++)
+            {
+                Console.WriteLine(members[i].StudentID);
+            }
+
+            context.Members.AddOrUpdate(m => new { m.AssociatedClub, m.approved, m.StudentID }, members.ToArray());
+            context.Clubs.AddOrUpdate(club => club.ClubName, new Club[]
+            {
+                new Club
+                {
+                    ClubName = "The Chess Club",
+                    CreationDate = new DateTime(day:23, month:10, year:2020),
+                    adminID = 1,
+                },
+                new Club
+               {
+                   ClubName = "Volley ball Club",
+                   CreationDate = new DateTime(day:01, month: 01, year: 2018),
+                   adminID = 1,
+               },
+                 new Club
+               {
+                   ClubName = "Martial Arts Club",
+                   CreationDate = new DateTime(day:15, month: 08, year: 2020),
+                   adminID = 1,
+               }
+
+            } // End of Clubs array
+                 );// End of Add or Update
+
+
+            context.SaveChanges();
+        }
+
+
+        private List<Member> get_members(Week5ClubContext context)
+        {
+            List<Student> students1 = GetStudents(context, 10);
+            List<Student> students2 = GetStudents(context, 10);
+            List<Student> students3 = GetStudents(context, 10);
+
+            return members;
+        }
+
+        private List<Student> GetStudents(Week5ClubContext context, int count)
+        {
+            // Create a random list of studnets ids
+            var randomSetStudent = context.Students.Select(s => new { s.StudentID, r = Guid.NewGuid() });
+            // Sort them and take 10
+            List<string> subset = randomSetStudent.OrderBy(s => s.r)
+                .Select(s => s.StudentID.ToString()).Take(count).ToList();
+            // Return the selected st students as a realized list
+            return context.Students.Where(s => subset.Contains(s.StudentID)).ToList();
+        }
 
         public static List<T> Get<T>(string resourceName)
         {
@@ -117,6 +200,3 @@
 
     }
 }
-
-
-// add clubs look at week 4 think I got it working there.
